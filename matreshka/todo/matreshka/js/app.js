e8d5541348d975ed3ab8da49ef32573b6b3fad4f
Andrey Gubanov <bookfast.org@gmail.com>
2014-07-15 11:50:11 +0400

    fixd typos in TODO docs

--- matreshka/todo/matreshka/js/app.js
+++ matreshka/todo/matreshka/js/app.js
@@ -1,10 +1,10 @@
 "use strict";
-// Key code variables definition as TodoMVC cpecification says then waiting for dom ready event.
+// Key code variables definition as TodoMVC cpecification says then waiting for DOM ready event.
 var ENTER_KEY = 13;
 var ESC_KEY = 27;
 
 $( function() {
-	// We're creating custom binder that will switch bound element visibility depending on property value by using [jQuery.fn.toggle](http://api.jquery.com/toggle/) method.
+	// We're creating custom binder that will switch bound element visibility depending on property value using [jQuery.fn.toggle](http://api.jquery.com/toggle/) method.
 	// ```js
 	// this.bindElement( 'x', element, MK.binders.display() );
 	// this.x = false; // element is hidden
--- matreshka/todo/matreshka/js/todos.js
+++ matreshka/todo/matreshka/js/todos.js
@@ -1,9 +1,9 @@
-// **todos.js** is the biggest file that contains most of logic because application is tiny. In bigger projects is better to split up your application parts. ``Todos`` class extends ``MK.Array`` class which means "collection".
+// **todos.js** is the biggest file that contains most of logic. In bigger projects is better to split up your application to a few JS files. ``Todos`` class extends ``MK.Array`` class which means "collection".
 "use strict";
 this.Todos = Class({
 	'extends': MK.Array,
 	Model: Todo,
-	// [itemRenderer](http://finom.github.io/matreshka/docs/Matreshka.Array.html#itemRenderer) method renders element for each added item. At this case it takes template in element that has ``id="todo_item_template"``
+	// [itemRenderer](http://finom.github.io/matreshka/docs/Matreshka.Array.html#itemRenderer) method renders element for each added item. At this case it retrieves HTML template for todo item from element that has ``id="todo_item_template"`` (look at HTML code)
 	itemRenderer: function() {
 		return $( '#todo_item_template' ).html();
 	},
@@ -11,15 +11,15 @@ this.Todos = Class({
 		var self = this
 			// Matreshka initialization by [initMK](http://finom.github.io/matreshka/docs/Matreshka.Array.html#initMK) method.
 			.initMK()
-			// ``"leftLength"`` is dependent from ``"length"`` and ``"completedLength"`` properties as their difference and listens any cnanges on these properties.
+			// ``"leftLength"`` is dependent from ``"length"`` and ``"completedLength"`` properties as their difference. It listens any cnanges on these properties.
 			.addDependency( 'leftLength', 'length completedLength', function( length, completedLength ) {
 				return length - completedLength;
 			})
-			// ``"bindings"`` method created bindings between instance properties and DOM elements. ``"events"`` as you might guess adds events listeners for the instance. I don't say that these method names mean somethig special, I just moved grouped actions to methods that I liked to call ``"bindings"`` and ``"events"``. After calling them we're parsing locally stored todos and passing obtained array to createFrom method which pushes converted array items to the instance.
+			// ``"bindings"`` method creates bindings between instance properties and DOM elements. ``"events"`` as you might guess adds event listeners to the instance. I don't say that these method names mean somethig special, I just moved grouped actions to methods that I liked to call ``"bindings"`` and ``"events"``. After calling them we're parsing locally stored todos and passing obtained array to [createFrom](http://finom.github.io/matreshka/docs/Matreshka.Array.html#createFrom) method which pushes converted array items to the instance.
 			.bindings()
 			.events()
 			.createFrom( JSON.parse( localStorage[ 'todos-matreshka' ] || '[]' ) )
-			// After we created todos array from ``localStorage`` we're adding event lister on change of ``"JSON"`` property which stores stringified representation of our todo list. Look at [procrastinate](http://finom.github.io/matreshka/docs/Matreshka.html#procrastinate) method. It disallows to call function many times per time period. We need this optimiation because ``localStorage`` stores data on hard drive which works much slower then RAM.
+			// After we created todos array retrieved from ``localStorage`` we're adding event lister on change of ``"JSON"`` property which stores stringified representation of our todo list. Look at [procrastinate](http://finom.github.io/matreshka/docs/Matreshka.html#procrastinate) method. It disallows to call function many times per time period. We need this optimiation because ``localStorage`` stores data on hard drive which works much slower then RAM.
 			.on( 'change:JSON', MK.procrastinate( function( evt ) {
 				localStorage[ 'todos-matreshka' ] = evt.value;
 			}) )
@@ -38,7 +38,7 @@ this.Todos = Class({
 	// **Bindings**
 	bindings: function() {
 		return this
-			// We're binding main alament which has "todoapp" ID. After that we'r binding another elements (main, footer etc).
+			// We're binding main element which has "todoapp" ID. This means association between instance and element. After that we're binding another elements (main, footer etc).
 			.bindElement( this, '#todoapp' )
 			.bindElement({
 				main: '#main',
@@ -96,7 +96,7 @@ this.Todos = Class({
 					this.newTodo = '';
 				}
 			})
-			// Code abowe shows that ``"allCompleted"`` property is associated with ``#toggle-all`` element. When it's value is changed we're setting same ``"completed"`` peoperty value for all items we have. ``"silent"`` flag says that ``"change:completed"`` event has not to be fired.
+			// Code above shows that ``"allCompleted"`` property is associated with ``#toggle-all`` element. When it's value is changed we're setting same ``"completed"`` peoperty value for all items we have. ``"silent"`` flag says that ``"change:completed"`` event has not to be fired.
 			.on( 'change:allCompleted', function( evt ) {
 				this.forEach( function( todo ) {
 					todo.set( 'completed', evt.value, { silent: true });
@@ -116,7 +116,7 @@ this.Todos = Class({
 			.on( '@click::destroy', function( evt ) {
 				this.pull( this.indexOf( evt.self ) );
 			})
-			// This event handler is attached on two events. The first is ``"modify"`` event which is firing when ``MK.Array`` collection changes (some element(s) is added or removed). The second is ``"@change:completed"`` event. "@" symbol means that we're listening ``"change:completed"`` for each todo item. So event handler executes when some todos are added or removed and when ``"completed"`` property is changed in some todo item. The code inside event handler explains itself: ``"allCompleted"`` becames ``true`` if every item is completed becames and becames ``false`` when some item is not completed. ``"completedLength"`` contains the length of completed items.
+			// This event handler is attached on two events. The first is ``"modify"`` event which is firing when ``MK.Array`` collection changes (some element(s) is added or removed). The second is ``"@change:completed"`` event. "@" symbol means that we're listening ``"change:completed"`` for each todo item. So event handler executes when some todos are added or removed and when ``"completed"`` property is changed on some todo item. The code inside event handler explains itself: ``"allCompleted"`` becames ``true`` if every item is completed becames and becames ``false`` when some item is not completed. ``"completedLength"`` contains the length of completed items.
 			.on( 'modify @change:completed', function() {
 				this.set( 'allCompleted', this.every( function( todo ) {
 					return todo.completed;
@@ -130,7 +130,7 @@ this.Todos = Class({
 			.on( 'modify @change:completed change:allCompleted', function() {
 				this.JSON = JSON.stringify( this );
 			})
-			// Next strings control which todo items are visible depends on hash URL. This part could be written few ways. I've choose [addDependency](http://finom.github.io/matreshka/docs/Matreshka.html#addDependency) way for it. What is going on there? We're listening ``"add"`` event. Fired event passes ```"added"``` property to the event (``evt``) argument which contain an array of added items. We're iterating new added todos and adding dependency over them. ``"visible"`` value depends on ``todos.route`` property and on self's ``"completed"`` property as third argument describes. 
+			// Next strings control which todo items are visible depends on ``location.hash``. This part could be written few ways. I've choose [addDependency](http://finom.github.io/matreshka/docs/Matreshka.html#addDependency) way for it. What is going on there? We're listening ``"add"`` event. Fired event passes ```"added"``` property to the event (``evt``) argument which contain an array of added items. We're iterating new added todos and adding dependency over them. ``"visible"`` value depends on ``todos.route`` property and on self's ``"completed"`` property as third argument describes. 
 			.on( 'add', function( evt ) {
 				evt.added.forEach( function( todo ) {
 					todo.addDependency( 'visible', [
